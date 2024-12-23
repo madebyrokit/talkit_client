8df7c225b9e688be1b7e6dc32f084cc8daa7f41e
simple3367@gmail.com <simple3367@gmail.com>
2024-12-23 17:25:26 +0900

    v1.0

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,7 +7,7 @@ import NavModal from "./NavModel";
 const NavBar = () => {
   const navigate = useNavigate();
   const { isLoggedIn, setIsLoggedIn } = useAuth();
-  
+
   const [modal, setModal] = useState(false);
 
   const homeHandler= () =>{navigate('/')}
@@ -21,7 +21,7 @@ const NavBar = () => {
     <nav className={styles.navBar}>
     
         <div className={styles.nav_header} type="button">
-            <span type="button" className={styles.logotext} onClick={homeHandler}>Talkit</span>
+            <span type="button" className={styles.logotext} onClick={homeHandler}>1Talkit</span>
         </div>
 
         <div className={styles.nav_body}>
